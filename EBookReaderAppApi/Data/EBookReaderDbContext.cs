3844d50d992459b80a091174b8c924dd44ca216a
Oleksii Akishev <akishev.oleksii@gmail.com>
2025-06-03 14:42:20 +0200

    Implemented EBookReaderDbContext and added it to the middleware

--- EBookReaderAppApi/Data/EBookReaderDbContext.cs
+++ EBookReaderAppApi/Data/EBookReaderDbContext.cs
@@ -1,9 +1,34 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using EBookReaderAppApi.Data.Entities;
+using Microsoft.EntityFrameworkCore;
 
 namespace EBookReaderAppApi.Data
 {
 	public class EBookReaderDbContext(DbContextOptions<EBookReaderDbContext> options) : DbContext(options)
 	{
+		public DbSet<User> Users { get; set; }
+		public DbSet<Library> Libraries { get; set; }
+		public DbSet<Book> Books { get; set; }
+		public DbSet<Page> Pages { get; set; }
 
+		protected override void OnModelCreating(ModelBuilder modelBuilder)
+		{
+			modelBuilder.Entity<User>()
+				.HasMany(u => u.Libraries)
+				.WithOne(l => l.User)
+				.HasForeignKey(l => l.UserId)
+				.OnDelete(DeleteBehavior.Cascade);
+
+			modelBuilder.Entity<Library>()
+				.HasMany(l => l.Books)
+				.WithOne(b => b.Library)
+				.HasForeignKey(b => b.LibraryId)
+				.OnDelete(DeleteBehavior.Cascade);
+
+			modelBuilder.Entity<Book>()
+				.HasMany(b => b.Pages)
+				.WithOne(p => p.Book)
+				.HasForeignKey(b => b.BookId)
+				.OnDelete(DeleteBehavior.Cascade);
+		}
 	}
 }
